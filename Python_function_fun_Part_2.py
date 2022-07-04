3cbee699e7aab23e6a929a8cc93572187af17817
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-07-03 23:36:53 -0700

    Python_function_fun_Part_3

--- Python_function_fun_Part_2.py
+++ Python_function_fun_Part_2.py
@@ -1,11 +1,15 @@
 
-# 1 arb_args
+# 1 arb_args  - Takes in any number of arguments and prints them one 
+# at a time.
 def arb_args (*movies):
     print(movies)
 
 arb_args("Apple", "Orange", "Pineapple")
 
-# 2 inner_function
+# 2 inner_functionTakes in two integers. Two nested functions should 
+# perform separate, distinct math operations using the integers. The 
+# result of both nested functions should then be added together and 
+# printed.
 def inner_func(num):
 
     # inner function definition
@@ -20,7 +24,9 @@ def inner_func(num):
 print(inner_func(5))
 
 
-# 3 default args
+# 3 lunch_lady - Takes in two strings: a student's name and their lunch
+#  preference. The function should print both strings. If a lunch 
+# preference is not given, "Mystery Meat" should be printed instead.
 def lunch_lady(name, meal_pref="Mystery Meat"):
     print (name, meal_pref)
 
@@ -29,7 +35,8 @@ lunch_lady("John")
 # overriting default with second argument
 lunch_lady("Amy", "Pizza")
 
-# 4 Multiple returns
+# 4 Multiple returns sum_n_product - Accepts two integers and returns both 
+# the sum and the product.
 def sum_n_product(num1, num2):
     new_sum = num1 + num2
     new_prod = num1 * num2
@@ -37,27 +44,31 @@ def sum_n_product(num1, num2):
 
 print(sum_n_product(4,6))
 
-# 5 alias_args
+# 5 alias_arb_args - Should be arb_args but assigned an alias. Remember, 
+# variables can hold functions in Python just like they can in JS. 
+# Alternatively, you can call a function from inside another function.
+alias_arb_args = arb_args
 
+#6 arb_mean - Accepts any number of integers and prints their average.
+def arb_mean(*args):
+    sum = 0
+    for i in args:
+        sum = sum + i
+    return sum/(len(args))
+    
 
+print("The mean for 2,6,8,11 is:")
+print(arb_mean(2,6,8,11))
 
+#7 arb_longest_string - Accepts any number of strings and returns the 
+# longest one.
 
+def arb_longest_string(*strings):
+    longest_string = ""
+    for i in strings:
+        if len(i)>len(longest_string):
+            longest_string = i
+    return i   
 
-
-#recursion
-
-def find_fact(num):
-
-    #base case
-    if num ==1:
-        return 1
-
-
-    else:
-        return (num * find_fact(num-1))
-
-n=5
-
-print(find_fact(n))
-
-
+print("The longest string is:")
+print(arb_longest_string("apples", "oranges","bananas", "pineapples"))
---
+++ Python_function_fun_Part_3.py
@@ -0,0 +1,34 @@
+# 1 name_args - Accepts any number of named arguments and 
+# prints them in the pattern key : value one at a time.
+
+def name_args(**names):
+    print(names)
+
+
+name_args(a="apples", b="oranges", c="bananas", d="kiwis")
+
+# 2 all_true - Returns True if all the elements in an iterable 
+# are true. Hint: there is an existing built-in function that 
+# could be very helpful here.
+
+
+
+# 3 one_true - Returns True if one of the elements in an 
+# iterable is true. Hint: there is an existing built-in function 
+# that could be very helpful here.
+
+
+
+# 4 recursive_factorial - Uses recursion to find the factorial of 
+# an integer.
+
+
+
+# 5 recursive_deduplicate - Recursively removes all adjacent 
+# duplicate letters from a string.
+#Input: AABBCCDD
+#Output: ABCD
+
+
+
+# 6 recursive_reverse - Uses recursion to reverse a string.
